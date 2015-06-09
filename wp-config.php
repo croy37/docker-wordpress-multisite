507db5634c283baba574bb872d77f95ee43728e5
croy37 <croy37@gmail.com>
2015-06-09 14:59:29 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -24,6 +24,6 @@ define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
-define('PATH_CURRENT_SITE', '/app');
+define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
